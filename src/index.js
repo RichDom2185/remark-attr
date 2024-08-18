f85cddd83e63a4cebd86ee6d1abf4ab07558969e
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-08-18 21:48:20 +0800

    Reformat code with Prettier

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
-'use strict';
+"use strict";
 
-const parseAttr = require('md-attr-parser');
-const htmlElemAttr = require('html-element-attributes');
-const isWhiteSpace = require('is-whitespace-character');
+const parseAttr = require("md-attr-parser");
+const htmlElemAttr = require("html-element-attributes");
+const isWhiteSpace = require("is-whitespace-character");
 
-const supportedElements = new Set(['link', 'atxHeading', 'strong', 'emphasis', 'deletion', 'code', 'setextHeading', 'fencedCode', 'reference', 'footnoteCall', 'autoLink']);
-const blockElements = new Set(['atxHeading', 'setextHeading']);
-const particularElements = new Set(['fencedCode']);
+const supportedElements = new Set([
+  "link",
+  "atxHeading",
+  "strong",
+  "emphasis",
+  "deletion",
+  "code",
+  "setextHeading",
+  "fencedCode",
+  "reference",
+  "footnoteCall",
+  "autoLink",
+]);
+const blockElements = new Set(["atxHeading", "setextHeading"]);
+const particularElements = new Set(["fencedCode"]);
 
 const particularTokenize = {};
 
-const DOMEventHandler = require('./dom-event-handler.js');
+const DOMEventHandler = require("./dom-event-handler.js");
 
 /* Table convertion between type and HTML tagName */
 const convTypeTag = {
-  image: 'img',
-  link: 'a',
-  heading: 'h1',
-  strong: 'strong',
-  emphasis: 'em',
-  delete: 's',
-  inlineCode: 'code',
-  code: 'code',
-  linkReference: 'a',
-  '*': '*',
+  image: "img",
+  link: "a",
+  heading: "h1",
+  strong: "strong",
+  emphasis: "em",
+  delete: "s",
+  inlineCode: "code",
+  code: "code",
+  linkReference: "a",
+  "*": "*",
 };
 
 /* This function is a generic function that transform
@@ -53,7 +65,7 @@ function tokenizeGenerator(prefix, oldParser, config) {
 
     let index = 0;
     let parsedAttr;
-    const {length} = value;
+    const { length } = value;
 
     if (!eaten || !eaten.position) {
       return undefined;
@@ -64,20 +76,23 @@ function tokenizeGenerator(prefix, oldParser, config) {
     index = eaten.position.end.offset - eaten.position.start.offset;
 
     // Then we check for attributes
-    if (index + prefix.length < length && value.charAt(index + prefix.length) === '{') {
-    // If any, parse it
+    if (
+      index + prefix.length < length &&
+      value.charAt(index + prefix.length) === "{"
+    ) {
+      // If any, parse it
       parsedAttr = parseAttr(value, index + prefix.length, config.mdAttrConfig);
     }
 
     // If parsed configure the node
     if (parsedAttr) {
-      if (config.scope && config.scope !== 'none') {
+      if (config.scope && config.scope !== "none") {
         const filtredProp = filterAttributes(parsedAttr.prop, config, type);
         if (filtredProp !== {}) {
           if (eaten.data) {
             eaten.data.hProperties = filtredProp;
           } else {
-            eaten.data = {hProperties: filtredProp};
+            eaten.data = { hProperties: filtredProp };
           }
         }
       }
@@ -100,8 +115,12 @@ function tokenizeModifierGenerator(oldParser, config) {
 
     let index = 0;
 
-    if (!eaten || !eaten.position ||
-        !eaten.children || eaten.children.length <= 0) {
+    if (
+      !eaten ||
+      !eaten.position ||
+      !eaten.children ||
+      eaten.children.length <= 0
+    ) {
       return eaten;
     }
 
@@ -109,12 +128,15 @@ function tokenizeModifierGenerator(oldParser, config) {
 
     const lastChild = eaten.children[eaten.children.length - 1];
 
-    if (!lastChild.value || lastChild.value.length <= 0 ||
-        lastChild.value[lastChild.value.length - 1] !== '}') {
+    if (
+      !lastChild.value ||
+      lastChild.value.length <= 0 ||
+      lastChild.value[lastChild.value.length - 1] !== "}"
+    ) {
       return eaten;
     }
 
-    index = lastChild.value.lastIndexOf('{');
+    index = lastChild.value.lastIndexOf("{");
 
     if (index <= 0) {
       return eaten;
@@ -137,13 +159,13 @@ function tokenizeModifierGenerator(oldParser, config) {
 
     // If parsed configure the node
     if (parsedAttr) {
-      if (config.scope && config.scope !== 'none') {
+      if (config.scope && config.scope !== "none") {
         const filtredProp = filterAttributes(parsedAttr.prop, config, type);
         if (filtredProp !== {}) {
           if (eaten.data) {
             eaten.data.hProperties = filtredProp;
           } else {
-            eaten.data = {hProperties: filtredProp};
+            eaten.data = { hProperties: filtredProp };
           }
         }
       }
@@ -160,65 +182,73 @@ function tokenizeModifierGenerator(oldParser, config) {
 
 // A generic function to parse attributes
 function filterAttributes(prop, config, type) {
-  const {scope} = config;
-  const {extend} = config;
-  const {allowDangerousDOMEventHandlers} = config;
+  const { scope } = config;
+  const { extend } = config;
+  const { allowDangerousDOMEventHandlers } = config;
   const specific = htmlElemAttr;
 
-  const extendTag = (extend => {
+  const extendTag = ((extend) => {
     const t = {};
-    Object.getOwnPropertyNames(extend).forEach(p => {
+    Object.getOwnPropertyNames(extend).forEach((p) => {
       t[convTypeTag[p]] = extend[p];
     });
     return t;
   })(extend);
 
   // Delete empty key/class/id attributes
-  Object.getOwnPropertyNames(prop).forEach(p => {
-    if (p !== 'key' && p !== 'class' && p !== 'id') {
-      prop[p] = prop[p] || '';
+  Object.getOwnPropertyNames(prop).forEach((p) => {
+    if (p !== "key" && p !== "class" && p !== "id") {
+      prop[p] = prop[p] || "";
     }
   });
 
-  const isDangerous = p => DOMEventHandler.includes(p);
-  const isSpecific = p => type in specific && specific[type].includes(p);
-  const isGlobal = p => htmlElemAttr['*'].includes(p) || p.match(/^aria-[a-z][a-z.-_\d]*$/) || p.match(/^data-[a-z][a-z_.-0-9]*$/);
+  const isDangerous = (p) => DOMEventHandler.includes(p);
+  const isSpecific = (p) => type in specific && specific[type].includes(p);
+  const isGlobal = (p) =>
+    htmlElemAttr["*"].includes(p) ||
+    p.match(/^aria-[a-z][a-z.-_\d]*$/) ||
+    p.match(/^data-[a-z][a-z_.-0-9]*$/);
 
   let inScope = () => false;
 
   // Function used to `or combine` two other function.
-  const orFunc = (fun, fun2) => x => fun(x) || fun2(x);
+  const orFunc = (fun, fun2) => (x) => fun(x) || fun2(x);
 
   // Respect the scope configuration
   switch (scope) {
-    case 'none': // Plugin is disabled
+    case "none": // Plugin is disabled
       break;
-    case 'permissive':
-    case 'every':
+    case "permissive":
+    case "every":
       if (allowDangerousDOMEventHandlers) {
         inScope = () => true;
       } else {
-        inScope = x => !isDangerous(x);
+        inScope = (x) => !isDangerous(x);
       }
 
       break;
-    case 'extended':
+    case "extended":
     default:
-      inScope = p => extendTag && type in extendTag && extendTag[type].includes(p);
-      inScope = orFunc(inScope, p => '*' in extendTag && extendTag['*'].includes(p));
-      // Or if it in the specific scope, fallthrough
-    case 'specific':
+      inScope = (p) =>
+        extendTag && type in extendTag && extendTag[type].includes(p);
+      inScope = orFunc(
+        inScope,
+        (p) => "*" in extendTag && extendTag["*"].includes(p)
+      );
+    // Or if it in the specific scope, fallthrough
+    case "specific":
       inScope = orFunc(inScope, isSpecific);
-      // Or if it in the global scope fallthrough
-    case 'global':
+    // Or if it in the global scope fallthrough
+    case "global":
       inScope = orFunc(inScope, isGlobal);
-      if (allowDangerousDOMEventHandlers) { // If allowed add dangerous attributes to global scope
+      if (allowDangerousDOMEventHandlers) {
+        // If allowed add dangerous attributes to global scope
         inScope = orFunc(inScope, isDangerous);
       }
   }
 
   // If an attributes isn't in the scope, delete it
-  Object.getOwnPropertyNames(prop).forEach(p => {
+  Object.getOwnPropertyNames(prop).forEach((p) => {
     if (!inScope(p)) {
       delete prop[p];
     }
@@ -234,7 +264,7 @@ function filterAttributes(prop, config, type) {
  * It's only temporary
  */
 function tokenizeFencedCode(oldParser, config) {
-  const prefix = '\n';
+  const prefix = "\n";
   function token(eat, value, silent) {
     // This we call the old tokenize
     const self = this;
@@ -260,20 +290,23 @@ function tokenizeFencedCode(oldParser, config) {
         // If it's an old version, we can still find from the attributes
         // from 'value' ¯\_(ツ)_/¯
         // Bad hack, will be deleted soon
-        parsedAttr = parseAttr(value, value.indexOf(' '));
+        parsedAttr = parseAttr(value, value.indexOf(" "));
       }
     }
 
     // If parsed configure the node
     if (parsedAttr) {
-      if (config.scope && config.scope !== 'none') {
+      if (config.scope && config.scope !== "none") {
         const filtredProp = filterAttributes(parsedAttr.prop, config, type);
 
         if (filtredProp !== {}) {
           if (eaten.data) {
-            eaten.data.hProperties = {...eaten.data.hProperties, ...filtredProp};
+            eaten.data.hProperties = {
+              ...eaten.data.hProperties,
+              ...filtredProp,
+            };
           } else {
-            eaten.data = {hProperties: filtredProp};
+            eaten.data = { hProperties: filtredProp };
           }
         }
       }
@@ -305,40 +338,48 @@ function remarkAttr(userConfig) {
     allowDangerousDOMEventHandlers: false,
     elements: supportedElements,
     extend: {},
-    scope: 'extended',
+    scope: "extended",
     mdAttrConfig: undefined,
     enableAtxHeaderInline: true,
     disableBlockElements: false,
   };
-  const config = {...defaultConfig, ...userConfig};
+  const config = { ...defaultConfig, ...userConfig };
 
   if (!isRemarkParser(parser)) {
-    throw new Error('Missing parser to attach `remark-attr` [link] (to)');
+    throw new Error("Missing parser to attach `remark-attr` [link] (to)");
   }
 
   const tokenizers = parser.prototype.inlineTokenizers;
   const tokenizersBlock = parser.prototype.blockTokenizers;
 
   // For each elements, replace the old tokenizer by the new one
-  config.elements.forEach(element => {
-    if ((element in tokenizersBlock || element in tokenizers) &&
-        supportedElements.has(element)) {
+  config.elements.forEach((element) => {
+    if (
+      (element in tokenizersBlock || element in tokenizers) &&
+      supportedElements.has(element)
+    ) {
       if (!config.disableBlockElements && blockElements.has(element)) {
         const oldElement = tokenizersBlock[element];
-        tokenizersBlock[element] = tokenizeGenerator('\n', oldElement, config);
+        tokenizersBlock[element] = tokenizeGenerator("\n", oldElement, config);
       } else if (particularElements.has(element)) {
         const oldElement = tokenizersBlock[element];
-        tokenizersBlock[element] = particularTokenize[element](oldElement, config);
+        tokenizersBlock[element] = particularTokenize[element](
+          oldElement,
+          config
+        );
       } else {
         const oldElement = tokenizers[element];
-        const elementTokenize = tokenizeGenerator('', oldElement, config);
+        const elementTokenize = tokenizeGenerator("", oldElement, config);
         elementTokenize.locator = tokenizers[element].locator;
         tokenizers[element] = elementTokenize;
       }
 
-      if (config.enableAtxHeaderInline && element === 'atxHeading') {
+      if (config.enableAtxHeaderInline && element === "atxHeading") {
         const oldElement = tokenizersBlock[element];
-        tokenizersBlock[element] = tokenizeModifierGenerator(oldElement, config);
+        tokenizersBlock[element] = tokenizeModifierGenerator(
+          oldElement,
+          config
+        );
       }
     }
   });
@@ -347,10 +388,9 @@ function remarkAttr(userConfig) {
 function isRemarkParser(parser) {
   return Boolean(
     parser &&
-    parser.prototype &&
-    parser.prototype.inlineTokenizers &&
-    parser.prototype.inlineTokenizers.link &&
-    parser.prototype.inlineTokenizers.link.locator,
+      parser.prototype &&
+      parser.prototype.inlineTokenizers &&
+      parser.prototype.inlineTokenizers.link &&
+      parser.prototype.inlineTokenizers.link.locator
   );
 }
-
